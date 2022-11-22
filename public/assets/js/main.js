d24eaf4ee161417265af052ca70f27553c559cdc
Luciakestel <luciakestelpires@gmail.com>
2022-11-22 22:17:19 +0100

    clone my project to GitHub

---
+++ public/assets/js/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const menuHamb = document.querySelector('.js_menu_hamb');
+const menuNav = document.querySelector('.js_menu');
+
+function HandleClick(){
+    if(menuNav.classList.contains('collapse')){
+        menuNav.classList.remove('collapse');
+        menuNav.classList.add('menu');
+    } else {
+        menuNav.classList.add('collapse');
+    }
+}
+
+menuHamb.addEventListener(('click'), HandleClick);
+//# sourceMappingURL=main.js.map
